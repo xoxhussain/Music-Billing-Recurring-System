a184cacab6c1764a5b69a82d50bf3cd1ee22f22d
Hussain <mhussainabid2006@gmail.com>
2026-09-09 18:56:48 +0500

    PR (addressed reviews): move payment authorization logic to service.

--- app/controllers/payment_authorizations_controller.rb
+++ app/controllers/payment_authorizations_controller.rb
@@ -10,50 +10,17 @@ class PaymentAuthorizationsController < ApplicationController
   end
 
   def create
-    payment_authorization =
-      current_user.payment_authorization ||
-      current_user.build_payment_authorization
-
-    customer =
-      if payment_authorization.stripe_customer_id.present?
-        Stripe::Customer.retrieve(payment_authorization.stripe_customer_id)
-      else
-        Stripe::Customer.create(
-          email: current_user.email,
-          name: current_user.name
-        )
-      end
-
-    setup_intent = Stripe::SetupIntent.create(
-      customer: customer.id,
-      payment_method_types: [ "card" ]
-    )
-
-    payment_authorization.update!(
-      stripe_customer_id: customer.id,
-    )
+    setup_intent = PaymentAuthorizationService.new(current_user).create_setup_intent
 
     render json: { client_secret: setup_intent.client_secret }
   end
 
   def confirm
-    payment_authorization = current_user.payment_authorization
+    authorized = PaymentAuthorizationService
+      .new(current_user)
+      .confirm_setup_intent(params[:setup_intent_id])
 
-    setup_intent = Stripe::SetupIntent.retrieve(params[:setup_intent_id])
-
-    unless payment_authorization &&
-           setup_intent.customer == payment_authorization.stripe_customer_id
-      redirect_to payment_authorization_path,
-                  alert: "Invalid payment authorization."
-      return
-    end
-
-    if setup_intent.status == "succeeded"
-      payment_authorization.update!(
-        stripe_payment_method_id: setup_intent.payment_method,
-        authorized: true
-      )
-
+    if authorized
       redirect_to payment_authorization_path,
                   notice: "Payment authorization completed successfully."
     else
